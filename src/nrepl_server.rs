e0a5777e0361ca1fdfffeca06429afe8168d2208
Sasha <leo.pourcelot@protonmail.com>
2020-05-06 22:51:41 +0200

    Adds basic nrepl
    I noticed by testing the nrepl system provided by `clj` handles "non-valid" input
    that way. The way it returns data still has to be changed, though.
    
    Next commits will focus on making returned data "valid".

--- src/nrepl_server.rs
+++ src/nrepl_server.rs
@@ -2,10 +2,12 @@
 
 use std::{
     dbg,
-    io::{Read, Result},
-    net::{Ipv4Addr, SocketAddrV4, TcpListener},
+    io::{BufReader, Read, Write, Result},
+    net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
     str,
 };
+
+use crate::repl::Repl;
 
 const SERVER_PORT: u16 = 5555;
 
@@ -15,10 +17,18 @@ fn get_address(port: u16) -> SocketAddrV4 {
     SocketAddrV4::new(lh, port)
 }
 
-/// TODO: add a nice way to change the server port instead of always using the
-/// default one.
+// TODO: add a nice way to change the server port instead of always using the
+// default one.
 fn nrepl_default_address() -> SocketAddrV4 {
     get_address(SERVER_PORT)
+}
+
+/// Returns whether if the tcp stream is ended or not.
+fn is_empty(stream: &TcpStream) -> Result<bool> {
+    let mut peeked_data = [0; 1];
+    stream.peek(&mut peeked_data)?;
+    let is_empty = peeked_data[0] == 0;
+    Ok(is_empty)
 }
 
 pub struct Server;
@@ -29,20 +39,24 @@ impl Server {
         let addr = nrepl_default_address();
         let listener = TcpListener::bind(addr)?;
 
-        println!("Server started");
         let (mut stream, addr) = listener.accept()?;
-        println!("New client at {}", addr);
+
+        let mut repl = Repl::default();
 
         loop {
-            let mut input_buffer = [0; 128];
-            stream.read(&mut input_buffer);
-
-            if input_buffer[0] == 0 {
+            if is_empty(&stream)? {
                 break;
             }
 
-            let s = str::from_utf8(&input_buffer).unwrap();
-            println!("Got data from {}: {}", &addr, s);
+            let mut input = BufReader::new(&stream);
+
+            let next = Repl::read(&mut input);
+
+            let evaled_next = repl.eval(&next);
+
+            let to_send = format!("{}\n", evaled_next);
+
+            stream.write(to_send.as_bytes());
         }
 
         Ok(())
